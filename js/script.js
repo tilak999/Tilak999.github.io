fbd6cd89c11a4a52391aa28cecd82ba549633afe
Tilak  Sasmal <Tilak@Tilaks-MacBook.local>
2017-02-11 12:59:10 +0530

    added index.html and css

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$(".hello").click(function(){
+		$(".hello").toggleClass("fullWidth");
+		$(".welcome").toggleClass("full");
+		console.log("ok");
+	})
+
+	window.onscroll = function() {myFunction()};
+});
+
+var full_width = false;
+
+function myFunction() 
+{
+    if (document.body.scrollTop > 50 && full_width == false) 
+    {
+    	full_width = true;
+    	$(".hello").toggleClass("full");
+		$(".welcome").toggleClass("full");
+		setTimeout(function(){
+			$(".full-page").hide();
+		},500);
+
+		window.scrollTo(0,100);
+    }
+    else if (document.body.scrollTop < 50 && full_width) 
+    {
+    	full_width = false;
+    	$(".full-page").show();
+    	$(".hello").toggleClass("full");
+		$(".welcome").toggleClass("full");
+    }
+}
